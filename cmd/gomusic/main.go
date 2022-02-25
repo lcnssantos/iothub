82ab6b2a3bef2fd884e50d27f40016e16073c64f
Luciano Santos <luciano.ssants@gmail.com>
2022-02-25 00:14:05 -0300

    add database connection

--- cmd/gomusic/main.go
+++ cmd/gomusic/main.go
@@ -1,7 +1,19 @@
 package main
 
-import "github.com/lcnssantos/gomusic/config"
+import (
+	"log"
+
+	"github.com/lcnssantos/gomusic/config"
+	"github.com/lcnssantos/gomusic/internal/database"
+)
 
 func main() {
 	config.Validate()
+	db, err := database.GetConnection()
+
+	if err != nil {
+		log.Panicln("Error to connect to database")
+	}
+
+	defer db.Close()
 }
